37faec923cb128890c919e7dbedcdc0e95c2abce
diego281010 <diego.almeida03@epn.edu.ec>
2026-07-06 09:03:25 -0500

    reinicio

--- src/main/java/com/example/proyecto/Conexion.java
+++ src/main/java/com/example/proyecto/Conexion.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 import java.sql.Connection;
 
 public class Conexion {
-    private static final String url = "jdbc:postgresql://db.ayxwwfljwnpxtedayfgz.supabase.co:5432/postgres?sslmode=require";
-    private static final String user = "postgres";
+    private static final String url = "jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:6543/postgres";
+    private static final String user = "postgres.ayxwwfljwnpxtedayfgz";
     private static final String password = "#Grupal2026";
 
     public static Connection getConexion() throws SQLException {
